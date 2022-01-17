8c9facbcaa334f0929a87e89a491b1b283ec56d8
David Schnurr <schnurr@gmail.com>
2022-01-17 10:17:47 -0800

    move lcs to its own file with comment

---
+++ src/lcs.js
@@ -0,0 +1,76 @@
+/*
+This is a copied version of lcs.js from the jsondiffpatch package
+Our plugin needs it, but jsondiffpatch does not export it.
+*/
+
+function defaultMatch(array1, array2, index1, index2) {
+  return array1[index1] === array2[index2];
+}
+
+function lengthMatrix(array1, array2, match, context) {
+  var len1 = array1.length;
+  var len2 = array2.length;
+  var x = void 0,
+      y = void 0;
+
+  // initialize empty matrix of len1+1 x len2+1
+  var matrix = [len1 + 1];
+  for (x = 0; x < len1 + 1; x++) {
+    matrix[x] = [len2 + 1];
+    for (y = 0; y < len2 + 1; y++) {
+      matrix[x][y] = 0;
+    }
+  }
+  matrix.match = match;
+  // save sequence lengths for each coordinate
+  for (x = 1; x < len1 + 1; x++) {
+    for (y = 1; y < len2 + 1; y++) {
+      if (match(array1, array2, x - 1, y - 1, context)) {
+        matrix[x][y] = matrix[x - 1][y - 1] + 1;
+      } else {
+        matrix[x][y] = Math.max(matrix[x - 1][y], matrix[x][y - 1]);
+      }
+    }
+  }
+  return matrix;
+}
+
+function backtrack(matrix, array1, array2, context) {
+  var index1 = array1.length;
+  var index2 = array2.length;
+  var subsequence = {
+    sequence: [],
+    indices1: [],
+    indices2: []
+  };
+
+  while (index1 !== 0 && index2 !== 0) {
+    var sameLetter = matrix.match(array1, array2, index1 - 1, index2 - 1, context);
+    if (sameLetter) {
+      subsequence.sequence.unshift(array1[index1 - 1]);
+      subsequence.indices1.unshift(index1 - 1);
+      subsequence.indices2.unshift(index2 - 1);
+      --index1;
+      --index2;
+    } else {
+      var valueAtMatrixAbove = matrix[index1][index2 - 1];
+      var valueAtMatrixLeft = matrix[index1 - 1][index2];
+      if (valueAtMatrixAbove > valueAtMatrixLeft) {
+        --index2;
+      } else {
+        --index1;
+      }
+    }
+  }
+  return subsequence;
+}
+
+export default function lcs(array1, array2, match, context) {
+  var innerContext = context || {};
+  var matrix = lengthMatrix(array1, array2, match || defaultMatch, innerContext);
+  var result = backtrack(matrix, array1, array2, innerContext);
+  if (typeof array1 === 'string' && typeof array2 === 'string') {
+    result.sequence = result.sequence.join('');
+  }
+  return result;
+}
--- src/main.js
+++ src/main.js
@@ -1,76 +1,4 @@
-var defaultMatch = function defaultMatch(array1, array2, index1, index2) {
-  return array1[index1] === array2[index2];
-};
-
-var lengthMatrix = function lengthMatrix(array1, array2, match, context) {
-  var len1 = array1.length;
-  var len2 = array2.length;
-  var x = void 0,
-      y = void 0;
-
-  // initialize empty matrix of len1+1 x len2+1
-  var matrix = [len1 + 1];
-  for (x = 0; x < len1 + 1; x++) {
-    matrix[x] = [len2 + 1];
-    for (y = 0; y < len2 + 1; y++) {
-      matrix[x][y] = 0;
-    }
-  }
-  matrix.match = match;
-  // save sequence lengths for each coordinate
-  for (x = 1; x < len1 + 1; x++) {
-    for (y = 1; y < len2 + 1; y++) {
-      if (match(array1, array2, x - 1, y - 1, context)) {
-        matrix[x][y] = matrix[x - 1][y - 1] + 1;
-      } else {
-        matrix[x][y] = Math.max(matrix[x - 1][y], matrix[x][y - 1]);
-      }
-    }
-  }
-  return matrix;
-};
-
-var backtrack = function backtrack(matrix, array1, array2, context) {
-  var index1 = array1.length;
-  var index2 = array2.length;
-  var subsequence = {
-    sequence: [],
-    indices1: [],
-    indices2: []
-  };
-
-  while (index1 !== 0 && index2 !== 0) {
-    var sameLetter = matrix.match(array1, array2, index1 - 1, index2 - 1, context);
-    if (sameLetter) {
-      subsequence.sequence.unshift(array1[index1 - 1]);
-      subsequence.indices1.unshift(index1 - 1);
-      subsequence.indices2.unshift(index2 - 1);
-      --index1;
-      --index2;
-    } else {
-      var valueAtMatrixAbove = matrix[index1][index2 - 1];
-      var valueAtMatrixLeft = matrix[index1 - 1][index2];
-      if (valueAtMatrixAbove > valueAtMatrixLeft) {
-        --index2;
-      } else {
-        --index1;
-      }
-    }
-  }
-  return subsequence;
-};
-
-var lcs = {
-  get: function get(array1, array2, match, context) {
-    var innerContext = context || {};
-    var matrix = lengthMatrix(array1, array2, match || defaultMatch, innerContext);
-    var result = backtrack(matrix, array1, array2, innerContext);
-    if (typeof array1 === 'string' && typeof array2 === 'string') {
-      result.sequence = result.sequence.join('');
-    }
-    return result;
-  }
-};
+import lcs from './lcs';
 
 var ARRAY_REMOVE = 0;
 var ARRAY_MOVE = 3;
@@ -256,7 +184,7 @@ var diffFilter = function arraysDiffFilter(context) {
   // diff is not trivial, find the LCS (Longest Common Subsequence)
   var trimmed1 = array1.slice(commonHead, len1 - commonTail);
   var trimmed2 = array2.slice(commonHead, len2 - commonTail);
-  var seq = lcs.get(
+  var seq = lcs(
     trimmed1, trimmed2,
     matchItems,
     matchContext
